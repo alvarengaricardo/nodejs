c98eb395c788daa55171104bbd438bde0fa140b4
ricardo <alvarenga.r.ricardo@gmail.com>
2018-05-01 20:50:15 -0300

    rota get/id

--- server.js
+++ server.js
@@ -50,32 +50,96 @@ router.get('/', function(req, res){
 });
 
 
-//API´s
-//===============================================
+
+//API's:
+//==============================================================================
 
 //Rotas que terminarem com '/produtos' (servir: GET ALL & POST)
 router.route('/produtos')
 
-    /* 1) Método: Criar Produto (acessar em: POST http://localhost:8000/api/produtos) */
-    .post(function(req, res){
+    /* 1) Método: Criar Produto (acessar em: POST http://localhost:8000/api/produtos)  */
+    .post(function(req, res) {
         var produto = new Produto();
 
-        // setar campos
+        //Aqui vamos setar os campos do produto (via request):
         produto.nome = req.body.nome;
         produto.preco = req.body.preco;
         produto.descricao = req.body.descricao;
 
-        produto.save(function(error){
+        produto.save(function(error) {
             if(error)
-                res.send('Erro ao gravar...: '+error);
-            res.json({message: 'Produto cadastrado com Sucesso!'});
-        })
+                res.send('Erro ao tentar salvar o Produto....: ' + error);
+            
+            res.json({ message: 'Produto Cadastrado com Sucesso!' });
+        });
+    })
+
+    /* 2) Método: Selecionar Todos Produtos (acessar em: GET http://localhost:8000/api/produtos)  */
+    .get(function(req, res) {
+        Produto.find(function(error, produtos) {
+            if(error) 
+                res.send('Erro ao tentar Selecionar Todos os produtos...: ' + error);
+
+            res.json(produtos);
+        });
     });
 
+    //Rotas que irão terminar em '/produtos/:produto_id' (servir tanto para: GET, PUT & DELETE: id):
+    router.route('/produtos/:produto_id')
 
-// Definindo um padrão das rotas prefixadas: '/api'
+    /* 3) Método: Selecionar por Id: (acessar em: GET http://localhost:8000/api/produtos/:produto_id) */
+    .get(function (req, res) {
+        
+        //Função para poder Selecionar um determinado produto por ID - irá verificar se caso não encontrar um detemrinado
+        //produto pelo id... retorna uma mensagem de error:
+        Produto.findById(req.params.produto_id, function(error, produto) {
+            if(error)
+                res.send('Id do Produto não encontrado....: ' + error);
+
+            res.json(produto);
+        });
+    })
+
+    /* 4) Método: Atualizar por Id: (acessar em: PUT http://localhost:8000/api/produtos/:produto_id) */
+    .put(function(req, res) {
+
+        //Primeiro: para atualizarmos, precisamos primeiro achar 'Id' do 'Produto':
+        Produto.findById(req.params.produto_id, function(error, produto) {
+            if (error) 
+                res.send("Id do Produto não encontrado....: " + error);
+
+                //Segundo: 
+                produto.nome = req.body.nome;
+                produto.preco = req.body.preco;
+                produto.descricao = req.body.descricao;
+
+                //Terceiro: Agora que já atualizamos os dados, vamos salvar as propriedades:
+                produto.save(function(error) {
+                    if(error)
+                        res.send('Erro ao atualizar o produto....: ' + error);
+
+                    res.json({ message: 'Produto atualizado com sucesso!' });
+                });
+            });
+        })
+
+        /* 5) Método: Excluir por Id (acessar: http://localhost:8000/api/produtos/:produto_id) */
+        .delete(function(req, res) {
+            
+            Produto.remove({
+                _id: req.params.produto_id
+                }, function(error) {
+                    if (error) 
+                        res.send("Id do Produto não encontrado....: " + error);
+
+                    res.json({ message: 'Produto Excluído com Sucesso!' });
+                });
+            });
+
+
+//Definindo um padrão das rotas prefixadas: '/api':
 app.use('/api', router);
 
-//iniciando a aplicação (servidor):
+//Iniciando a Aplicação (servidor):
 app.listen(port);
 console.log("Iniciando a app na porta " + port);
